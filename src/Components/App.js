52cc2775351ce637a7b2f5a37b42d8ff667e323b
Grangeos <grangeraphael74@gmail.com>
2018-09-05 15:26:56 +0200

    do the graphic interface

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="pong">
+        <span className="leftBar">
+        </span>
+        <span className="center">
+        </span>
+        <span className="rightBar">
+        </span>
+        <span className="ball">
+        </span>
+        <span className="score">
+          5  2
+        </span>
+
+      </section>
+    );
+  }
+}
+
+export default App;
